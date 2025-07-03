4ce715a23021fe5d8562077956c62e5ea10f3fc8
ASHTHRON <chaurasiaayush374@gmail.com>
2025-07-03 15:37:16 +0530

    Delete railway booking directory

--- railway booking/login.js
+++
@@ -1,9 +0,0 @@
-const showPasswordCheckbox = document.getElementById('showPassword');
-const passwordInput = document.getElementById('password');
-showPasswordCheckbox.addEventListener('change', function() {
-    if (this.checked) {
-        passwordInput.type = 'text';
-    } else {
-        passwordInput.type = 'password';
-    }
-});
